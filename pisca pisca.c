83de105e46738a9aaf162781320d6a0418d2f493
Clara-Cubarenco <99342679+Clara-Cubarenco@users.noreply.github.com>
2022-03-23 12:42:28 -0300

    Add files via upload

---
+++ pisca pisca.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+   char e_estado, v_tempo;
+    printf("lampada apagada\n");
+    while(1){
+    printf("l_liga; d_desliga:\n");
+    
+}
+    return 0;
+}
